6099fcb672be8f28e9bbaf33d53376416facbd5a
Alex Diyan <alexdiyan.ad@gmail.com>
2025-01-09 15:43:34 +0000

    Update aws callback urls

--- src/util/authHandling.tsx
+++ src/util/authHandling.tsx
@@ -1,6 +1,7 @@
 export const signOutRedirect = () => {
+  const redirectUrl = import.meta.env.PROD ? "https://ambient-forest-admin.vercel.app/hub" : "http://localhost:5173/hub"
   const clientId = import.meta.env.VITE_AWS_CLIENT_ID
-  const logoutUri = "http://localhost:5173";
+  const logoutUri = redirectUrl
   const cognitoDomain = "https://eu-west-2g9hrpnzqd.auth.eu-west-2.amazoncognito.com";
   window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
 };
